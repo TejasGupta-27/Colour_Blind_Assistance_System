f7feaaf297f7625851273a188436164b2f044e58
TejasGupta-27 <b22cs093@iitj.ac.in>
2024-03-06 23:21:38 +0530

    added image classification

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,22 +1,29 @@
 import 'dart:io';
-import 'package:colour_blindness/welcome_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'auth_service.dart';
-import 'package:colour_blindness/profile_screen.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 
 class HomePage extends StatefulWidget {
-  final AuthService authService = AuthService();
-
-  HomePage({super.key});
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   File? _pickedImage;
+  late ImageLabeler _imageLabeler;
+
+  List<ImageLabel> _labels = [];
+  List<Color> _dominantColors = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeImageLabeler();
+  }
+
+  Future<void> _initializeImageLabeler() async {
+    _imageLabeler = GoogleMlKit.vision.imageLabeler();
+  }
 
   void _pickImage(ImageSource source) async {
     final pickedImage = await ImagePicker().pickImage(source: source);
@@ -25,115 +32,91 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         _pickedImage = File(pickedImage.path);
       });
-      _showSnackBar(context, 'Image selected successfully', true);
-    } else {
-      _showSnackBar(context, 'No image selected', false);
+      _runImageLabeling();
     }
   }
 
-  void _showSnackBar(BuildContext context, String message, bool success) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-        backgroundColor: success ? Colors.green : Colors.red,
-      ),
-    );
+  Future<void> _runImageLabeling() async {
+    if (_pickedImage == null) return;
+
+    try {
+      final inputImage = InputImage.fromFilePath(_pickedImage!.path);
+      final labels = await _imageLabeler.processImage(inputImage);
+      setState(() {
+        _labels = labels;
+      });
+      _extractDominantColors(inputImage);
+    } catch (e) {
+      print('Failed to run image labeling: $e');
+    }
+  }
+
+  void _extractDominantColors(InputImage inputImage) {
+    // Code to extract dominant colors goes here
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Color Blindness Simulator'),
-      ),
-      drawer: buildDrawer(context),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => _pickImage(ImageSource.gallery),
-              child: const Text('Choose Photo'),
-            ),
-            const SizedBox(height: 10.0),
-            ElevatedButton(
-              onPressed: () => _pickImage(ImageSource.camera),
-              child: const Text('Take Photo'),
-            ),
-            if (_pickedImage != null)
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Image.file(
-                  _pickedImage!,
-                  height: 150,
-                  width: 150,
-                  fit: BoxFit.cover,
-                ),
-              ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildDrawer(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
+ @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Image Labeling & Color Detection'),
+    ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          DrawerHeader(
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 99, 16, 114),
+          ElevatedButton(
+            onPressed: () => _pickImage(ImageSource.gallery),
+            child: const Text('Choose Photo'),
+          ),
+          const SizedBox(height: 10.0),
+          ElevatedButton(
+            onPressed: () => _pickImage(ImageSource.camera),
+            child: const Text('Take Photo'),
+          ),
+          if (_pickedImage != null)
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Image.file(
+                _pickedImage!,
+                height: 150,
+                width: 150,
+                fit: BoxFit.cover,
+              ),
             ),
-            child: FutureBuilder<User?>(
-              future: widget.authService.getCurrentUser(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                } else if (snapshot.hasData) {
-                  User user = snapshot.data!;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CircleAvatar(
-                        radius: 40,
-                        backgroundImage: NetworkImage(user.photoURL ?? ''),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        user.displayName ?? '',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  );
-                } else {
-                  return const Text('User not signed in');
-                }
-              },
+          if (_labels.isNotEmpty)
+            Column(
+              children: [
+                Text(
+                  'Top Answer: ${_labels.first.label}',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Confidence: ${(_labels.first.confidence * 100).toStringAsFixed(2)}%',
+                ),
+              ],
             ),
-          ),
-          ListTile(
-            title: const Text('Edit Profile'),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => const ProfileScreen()),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text('Sign Out'),
-            onTap: () async {
-              await widget.authService.signOut();
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => WelcomeScreen()),
-              );
-            },
-          ),
+          if (_dominantColors.isNotEmpty)
+            Column(
+              children: [
+                const SizedBox(height: 20),
+                Text(
+                  'Dominant Color:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Color: ${_dominantColors.first}',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
         ],
       ),
-    );
-  }
+    ),
+  );
+}
+
 }
